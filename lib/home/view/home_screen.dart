e703f8bcae81baafd8d903d1a0ade963901d7a25
WooGi1020 <jungwk1020@gmail.com>
2025-03-02 17:00:19 +0900

    feat: 임시 지정

---
+++ lib/home/view/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:gachtaxi_app/common/constants/colors.dart';
+import 'package:gachtaxi_app/common/layout/default_layout.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      child: SafeArea(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '홈',
+              style: TextStyle(color: AppColors.white),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
