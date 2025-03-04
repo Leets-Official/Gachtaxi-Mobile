81a73a74081f81b64d8db64fdea0f3b70d1f7553
WooGi1020 <jungwk1020@gmail.com>
2025-03-05 00:26:15 +0900

    feat: 홈스크린에 최종 적용

--- lib/domain/home/view/home_screen.dart
+++ lib/domain/home/view/home_screen.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:gachtaxi_app/common/constants/colors.dart';
 import 'package:gachtaxi_app/common/layout/default_layout.dart';
+import 'package:gachtaxi_app/domain/home/components/custom_bottom_nav_bar.dart';
+import 'package:gachtaxi_app/domain/home/components/custom_bottom_sheet.dart';
+import 'package:gachtaxi_app/domain/home/components/custom_top_bar.dart';
+import 'package:gachtaxi_app/domain/home/components/google_maps.dart';
+import 'package:gachtaxi_app/domain/home/view/tab-view/tab_view.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController? _tabController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController!.addListener(() {
+      setState(() {
+        _selectedIndex = _tabController!.index;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      bottomNavigationBar: CustomBottomNavBar(
+          tabController: _tabController, selectedIndex: _selectedIndex),
       child: SafeArea(
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Text(
-              '홈',
-              style: TextStyle(color: AppColors.white),
+            GoogleMaps(),
+            CustomTopBar(),
+            CustomBottomSheet(
+              child: TabView(tabController: _tabController!),
             ),
           ],
-        )),
+        ),
       ),
     );
   }
