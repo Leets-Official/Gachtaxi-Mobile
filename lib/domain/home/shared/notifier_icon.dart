b5d4bec9deb0cb68671e5f87ef622779ccc677d0
WooGi1020 <jungwk1020@gmail.com>
2025-03-05 00:25:58 +0900

    feat: 알림아이콘 클래스 작성

---
+++ lib/domain/home/shared/notifier_icon.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class NotifierIcon extends StatelessWidget {
+  const NotifierIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications,
+              color: Color(0xFF08F283),
+            )),
+        Positioned(
+          top: 10,
+          right: 12,
+          child: Stack(
+            children: [
+              Container(
+                width: 12,
+                height: 12,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xFFFF3636),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
