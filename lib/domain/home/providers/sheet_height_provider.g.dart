b65e202a41c60a5ee4ec1bbfc70e5da09215e8ea
WooGi1020 <jungwk1020@gmail.com>
2025-03-05 00:24:06 +0900

    feat: 바텀시트 높이 전역 상태로 정의

---
+++ lib/domain/home/providers/sheet_height_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sheet_height_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sheetHeightNotifierHash() =>
+    r'a31fb20cfbd7f07a5b3a4e939921c88edc523249';
+
+/// See also [SheetHeightNotifier].
+@ProviderFor(SheetHeightNotifier)
+final sheetHeightNotifierProvider = AutoDisposeNotifierProvider<
+    SheetHeightNotifier, BottomSheetHeightState>.internal(
+  SheetHeightNotifier.new,
+  name: r'sheetHeightNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sheetHeightNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SheetHeightNotifier = AutoDisposeNotifier<BottomSheetHeightState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
