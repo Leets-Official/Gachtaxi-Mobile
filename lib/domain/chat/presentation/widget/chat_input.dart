5ea85aa6f1de34b4b11533bd2d566871f3b33af9
hyxklee <ewgt1234@naver.com>
2025-03-05 15:38:44 +0900

    feat: 채팅 입력창 UI 구현

---
+++ lib/domain/chat/presentation/widget/chat_input.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gachtaxi_app/common/constants/colors.dart';
+import '../../../../common/constants/typography.dart';
+import '../state/chat_input_notifier.dart';
+
+class ChatInputField extends ConsumerStatefulWidget {
+  const ChatInputField({super.key});
+
+  @override
+  ChatInputFieldState createState() => ChatInputFieldState();
+}
+
+class ChatInputFieldState extends ConsumerState<ChatInputField> {
+  @override
+  Widget build(BuildContext context) {
+    final chatController = ref.watch(chatInputProvider);
+    final chatNotifier = ref.read(chatInputProvider.notifier);
+
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFF20271E),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 7.h),
+        child: Row(
+          children: [
+            // 추가 액션 버튼
+            Container(
+              width: 32.w,
+              height: 32.h,
+              decoration: BoxDecoration(
+                color: AppColors.secondaryLight,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: Icon(Icons.add, color: Colors.black),
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  // TODO: 파일 추가 기능
+                },
+              ),
+            ),
+
+            SizedBox(width: 7.w),
+
+            // 입력창
+            Expanded(
+              child: Container(
+                width: 250.w,
+                constraints: BoxConstraints(minHeight: 36.h),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF465443),
+                  borderRadius: BorderRadius.circular(18.r),
+                ),
+                child: TextField(
+                  controller: chatController,
+                  style: TextStyle(color: Colors.white, fontSize: 14.sp),
+                  cursorColor: AppColors.lightGray,
+                  cursorWidth: 1.5.w,
+                  cursorHeight: 16.h,
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  expands: false,
+                  decoration: InputDecoration(
+                    hintText: "메시지 입력",
+                    hintStyle: TextStyle(
+                      color: const Color(0xFF2A3328),
+                      fontSize: AppTypography.fontSizeSmall.sp,
+                    ),
+                    border: InputBorder.none,
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(vertical: 6.h, horizontal: 16.w),
+                  ),
+                  onSubmitted: (_) => chatNotifier.submitMessage(),
+                ),
+              ),
+            ),
+
+            SizedBox(width: 7.w),
+
+            // 보내기 버튼
+            Container(
+              width: 36.w,
+              height: 36.h,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: Icon(Icons.send, color: Colors.black),
+                padding: EdgeInsets.zero,
+                onPressed: chatNotifier.submitMessage,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
