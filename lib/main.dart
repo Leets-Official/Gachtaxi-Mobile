551f4ce452e5ac63b1f88589c8385eac2ede80f5
hyxklee <ewgt1234@naver.com>
2025-03-05 20:16:55 +0900

    feat: ScreenUtil 설정 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:gachtaxi_app/domain/home/view/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // ✅ Riverpod 추가
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'domain/chat/presentation/view/chat_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    ProviderScope(
+      child: ScreenUtilInit(
+        designSize: Size(360, 800),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return const MyApp();
+        },
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       theme: ThemeData(
         fontFamily: 'NotoSansKR',
       ),
-      home: HomeScreen(),
+      home: const ChatScreen(),
       debugShowCheckedModeBanner: false,
-    ),
-  );
+    );
+  }
 }
