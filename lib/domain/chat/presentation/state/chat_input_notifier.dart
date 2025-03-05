0e446ec96470613e17b6a84a477e7e10e49212c8
hyxklee <ewgt1234@naver.com>
2025-03-05 20:21:54 +0900

    refactor: import문 수정

--- lib/domain/chat/presentation/state/chat_input_notifier.dart
+++ lib/domain/chat/presentation/state/chat_input_notifier.dart
@@ -4,7 +4,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'chat_input_notifier.g.dart';
 
-
 @riverpod
 class ChatInputNotifier extends _$ChatInputNotifier {
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // ✅ Riverpod 추가
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'domain/chat/presentation/view/chat_screen.dart';
 
