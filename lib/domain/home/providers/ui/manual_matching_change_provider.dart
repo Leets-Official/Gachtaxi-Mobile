9a2a51d76db5d331c34a280d407c37186fa4fc2f
WooGi1020 <jungwk1020@gmail.com>
2025-03-10 00:06:57 +0900

    feat: 수동매칭 카테고리 UI 변경 프로바이더 작성

---
+++ lib/domain/home/providers/ui/manual_matching_change_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'manual_matching_change_provider.g.dart';
+
+@riverpod
+class ManualMatchingChangeNotifier extends _$ManualMatchingChangeNotifier {
+  @override
+  String build() {
+    return 'manual_matching';
+  }
+
+  void toggleCategory() {
+    state = state == 'manual_matching' ? 'my_matching' : 'manual_matching';
+  }
+}
---
+++ lib/domain/home/providers/ui/manual_matching_change_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'manual_matching_change_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$manualMatchingChangeNotifierHash() =>
+    r'71a2cc4c900e8b45a564e456f460923aaeef2047';
+
+/// See also [ManualMatchingChangeNotifier].
+@ProviderFor(ManualMatchingChangeNotifier)
+final manualMatchingChangeNotifierProvider =
+    AutoDisposeNotifierProvider<ManualMatchingChangeNotifier, String>.internal(
+  ManualMatchingChangeNotifier.new,
+  name: r'manualMatchingChangeNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$manualMatchingChangeNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ManualMatchingChangeNotifier = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
