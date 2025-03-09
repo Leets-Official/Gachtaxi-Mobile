e1854add611cc92e91ad197eda36bdec20e19e96
WooGi1020 <jungwk1020@gmail.com>
2025-03-10 00:07:22 +0900

    feat: 탭뷰 프로필 이동 로직 작성

--- lib/domain/home/view/tab-view/tab_view.dart
+++ lib/domain/home/view/tab-view/tab_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gachtaxi_app/common/util/slide_page_route.dart';
+import 'package:gachtaxi_app/domain/chat/presentation/view/chat_screen.dart';
 import 'package:gachtaxi_app/domain/home/view/tab-view/auto_matching_screen.dart';
 import 'package:gachtaxi_app/domain/home/view/tab-view/friend_list_screen.dart';
 import 'package:gachtaxi_app/domain/home/view/tab-view/manual_matching_screen.dart';
@@ -16,6 +18,38 @@ class TabView extends StatefulWidget {
 }
 
 class _TabViewState extends State<TabView> with SingleTickerProviderStateMixin {
+  int _previousIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.tabController.addListener(_handleTabChange);
+  }
+
+  @override
+  void dispose() {
+    widget.tabController.removeListener(_handleTabChange);
+    super.dispose();
+  }
+
+  void _handleTabChange() {
+    if (widget.tabController.index == 3) {
+      _previousIndex =
+          _previousIndex != 3 ? widget.tabController.previousIndex : 0;
+      Future.microtask(() {
+        if (!mounted) return;
+        Navigator.of(context).push(
+          SlidePageRoute(screen: ChatScreen()),
+        );
+        if (mounted) {
+          setState(() {
+            widget.tabController.index = _previousIndex; // 이전 탭으로 복귀
+          });
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return TabBarView(
