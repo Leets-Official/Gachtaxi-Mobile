7b7b4fb61d23d3d2ee70e8562c7ceac20a46c7a6
김지원 <jiwonkim0531@gachon.ac.kr>
2025-03-12 23:16:13 +0900

    feat: landing 전체 화면 UI 구현

--- lib/domain/landing/view/landing_screen.dart
+++ lib/domain/landing/view/landing_screen.dart
@@ -6,16 +6,26 @@ import 'package:gachtaxi_app/domain/landing/view/third_landing_screen.dart';
 import 'package:gachtaxi_app/common/widgets/custom_button.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:gachtaxi_app/common/constants/colors.dart';
+import 'package:gachtaxi_app/domain/sign-up/view/email_verification_screen.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<LandingScreen> {
   final PageController _pageController = PageController();
+
+  void _navigateToEmailVerification() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const EmailVerificationScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   text: "카카오로 시작하기",
                   backgroundColor: Color(0xFFFFE001),
                   textColor: Colors.black,
-                  onPressed: () {},
+                  onPressed: _navigateToEmailVerification,
                   icon: SvgPicture.asset('assets/icons/kakao_icon.svg', width: 20),
                 ),
                 const SizedBox(height: 10),
