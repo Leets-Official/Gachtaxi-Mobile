ad833740f2c3672d613c65f92de119b9884bf859
김지원 <jiwonkim0531@gachon.ac.kr>
2025-03-16 15:55:09 +0900

    fix: 불필요한 변수 삭제 및 공통 색상 적용

--- lib/common/components/input_field.dart
+++ lib/common/components/input_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gachtaxi_app/common/constants/colors.dart';
 import 'package:gachtaxi_app/common/constants/typography.dart';
 
@@ -6,9 +7,7 @@ class InputField extends StatefulWidget {
   final String label;
   final String hintText;
   final TextEditingController controller;
-  final bool isPassword;
   final TextInputType keyboardType;
-  final Widget? suffixIcon;
   final FocusNode? focusNode;
   final bool autoFocus;
   final Function(String)? onSubmitted;
@@ -18,9 +17,7 @@ class InputField extends StatefulWidget {
     required this.label,
     required this.hintText,
     required this.controller,
-    this.isPassword = false,
     this.keyboardType = TextInputType.text,
-    this.suffixIcon,
     this.focusNode,
     this.autoFocus = false,
     this.onSubmitted,
@@ -33,8 +30,6 @@ class InputField extends StatefulWidget {
 class _InputFieldState extends State<InputField> {
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -46,14 +41,13 @@ class _InputFieldState extends State<InputField> {
             fontWeight: AppTypography.fontWeightMedium,
           ),
         ),
-        SizedBox(height: screenWidth * 0.05),
+        SizedBox(height: 16.h),
 
         SizedBox(
-          height: 50,
+          height: 50.h,
           child: TextField(
             focusNode: widget.focusNode,
             controller: widget.controller,
-            obscureText: widget.isPassword,
             keyboardType: widget.keyboardType,
             autofocus: widget.autoFocus,
             style: TextStyle(
@@ -61,31 +55,27 @@ class _InputFieldState extends State<InputField> {
               fontSize: AppTypography.fontSizeSmall,
               fontWeight: AppTypography.fontWeightMedium,
             ),
-            onSubmitted: widget.onSubmitted, // ✅ 엔터 키 입력 이벤트 추가
+            onSubmitted: widget.onSubmitted,
             decoration: InputDecoration(
               filled: true,
               fillColor: AppColors.neutralDark,
               hintText: widget.hintText,
               hintStyle: TextStyle(
-                color: Color(0xFF787272),
+                color:  AppColors.inputGrey,
                 fontSize: AppTypography.fontSizeSmall,
                 fontWeight: AppTypography.fontWeightMedium,
               ),
               border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(color: Color(0xFF787272)),
+                borderRadius: BorderRadius.circular(15.r),
+                borderSide: const BorderSide(color: AppColors.inputGrey),
               ),
               enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(color: Color(0xFF787272)),
+                borderRadius: BorderRadius.circular(15.r),
+                borderSide: const BorderSide(color: AppColors.inputGrey),
               ),
               focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
+                borderRadius: BorderRadius.circular(15.r),
                 borderSide: const BorderSide(color: AppColors.primary),
-              ),
-              suffixIcon: widget.suffixIcon,
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: screenWidth * 0.04,
               ),
             ),
           ),
