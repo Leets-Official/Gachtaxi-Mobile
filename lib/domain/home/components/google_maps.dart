5bbb8004bf34f982f6fdf5d56dec9c4c7daf7917
WooGi1020 <jungwk1020@gmail.com>
2025-03-20 01:04:34 +0900

    feat: Consumer 변경 및 로직 적용

--- lib/domain/home/components/google_maps.dart
+++ lib/domain/home/components/google_maps.dart
@@ -1,30 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:gachtaxi_app/common/constants/location.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gachtaxi_app/domain/home/providers/ui/map_location_provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class GoogleMaps extends StatefulWidget {
+class GoogleMaps extends ConsumerStatefulWidget {
   const GoogleMaps({super.key});
 
   @override
-  State<GoogleMaps> createState() => _GoogleMapsState();
+  ConsumerState<GoogleMaps> createState() => _GoogleMapsState();
 }
 
-class _GoogleMapsState extends State<GoogleMaps> {
+class _GoogleMapsState extends ConsumerState<GoogleMaps> {
   GoogleMapController? _controller;
 
-  static final CameraPosition _gachonCenter = CameraPosition(
-    target: Location.centerOfgachon.position,
-    zoom: 16.7,
-  );
+  BitmapDescriptor? _departureIcon;
+  BitmapDescriptor? _destinationIcon;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCustomMarkers();
+  }
+
+  Future<void> _loadCustomMarkers() async {
+    final departureIcon = await BitmapDescriptor.asset(
+      const ImageConfiguration(size: Size(40, 40)),
+      'assets/icons/departure_marker.png',
+    );
+
+    final destinationIcon = await BitmapDescriptor.asset(
+      const ImageConfiguration(size: Size(40, 40)),
+      'assets/icons/destination_marker.png',
+    );
+
+    setState(() {
+      _departureIcon = departureIcon;
+      _destinationIcon = destinationIcon;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    LatLng center = ref.watch(mapLocationNotifierProvider).center.position;
+    LatLng departure =
+        ref.watch(mapLocationNotifierProvider).departure.position;
+    LatLng destination =
+        ref.watch(mapLocationNotifierProvider).destination.position;
+
+    if (_departureIcon == null || _destinationIcon == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
     return GoogleMap(
       zoomControlsEnabled: false,
       mapType: MapType.normal,
-      initialCameraPosition: _gachonCenter,
+      initialCameraPosition: CameraPosition(
+        target: center,
+        zoom: 15.5,
+      ),
       onMapCreated: (GoogleMapController controller) {
         _controller = controller;
+      },
+      markers: {
+        Marker(
+          markerId: const MarkerId('departure'),
+          position: departure,
+          icon: _departureIcon!,
+          infoWindow: const InfoWindow(title: "출발지"),
+        ),
+        Marker(
+          markerId: const MarkerId('destination'),
+          position: destination,
+          icon: _destinationIcon!,
+          infoWindow: const InfoWindow(title: "목적지"),
+        )
       },
     );
   }
