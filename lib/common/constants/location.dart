9dcef8f4473f979f0acbcc2055e464fb0e02fed3
WooGi1020 <jungwk1020@gmail.com>
2025-03-20 01:03:16 +0900

    feat: 출발, 목적지 위치 추가

--- lib/common/constants/location.dart
+++ lib/common/constants/location.dart
@@ -9,8 +9,20 @@ class Location {
 
   static final Location centerOfgachon = Location(
     "가천대 구글맵 시점",
-    37.44852635280086,
-    127.12855122490176,
+    37.449694962471774,
+    127.1310169248561,
+  );
+
+  static final Location basicDeparture = Location(
+    '가천대 정문',
+    37.45051487868865,
+    127.12689330709496,
+  );
+
+  static final Location basicDestination = Location(
+    '가천대 AI 공학관',
+    37.45532672771233,
+    127.13463780887568,
   );
 
   LatLng get position => LatLng(latitude, longitude);
