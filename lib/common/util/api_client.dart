6cddbf38846a6132034d8350ff21df84be0b285b
WooGi1020 <jungwk1020@gmail.com>
2025-04-01 01:45:21 +0900

    fix: api client 토큰 사용 로직 수정

--- lib/common/util/api_client.dart
+++ lib/common/util/api_client.dart
@@ -13,37 +13,40 @@ class ApiClient {
       headers: {"Content-Type": "application/json"},
     ),
   )..interceptors.add(
-    InterceptorsWrapper(
-      onRequest: (options, handler) async {
-        final token = await TokenStorage.loadAccessToken();
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final token = await TokenStorage.loadAccessToken();
 
-        if (token != null) {
-          options.headers["Authorization"] = token;
-        }
+          if (token != null) {
+            options.headers["Authorization"] = 'Bearer $token';
+          }
 
-        logger.i("📤 [API 요청] ${options.method} ${options.uri}");
-        logger.d("🔹 요청 헤더: ${options.headers}");
-        if (options.data != null) {
-          logger.d("🔹 요청 본문: ${options.data}");
-        }
+          logger.i("📤 [API 요청] ${options.method} ${options.uri}");
+          logger.d("🔹 요청 헤더: ${options.headers}");
+          if (options.data != null) {
+            logger.d("🔹 요청 본문: ${options.data}");
+          }
 
-        return handler.next(options);
-      },
-      onResponse: (response, handler) {
-        logger.i("📥 [API 응답] ${response.statusCode} ${response.requestOptions.uri}");
-        logger.d("🔹 응답 본문: ${response.data}");
-        return handler.next(response);
-      },
-      onError: (DioError error, handler) {
-        logger.e("❌ [API 오류] ${error.response?.statusCode} ${error.requestOptions.uri}");
-        logger.e("🔹 오류 응답: ${error.response?.data}");
-        return handler.next(error);
-      },
-    ),
-  );
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          logger.i(
+              "📥 [API 응답] ${response.statusCode} ${response.requestOptions.uri}");
+          logger.d("🔹 응답 본문: ${response.data}");
+          return handler.next(response);
+        },
+        onError: (DioException error, handler) {
+          logger.e(
+              "❌ [API 오류] ${error.response?.statusCode} ${error.requestOptions.uri}");
+          logger.e("🔹 오류 응답: ${error.response?.data}");
+          return handler.next(error);
+        },
+      ),
+    );
 
   // GET
-  static Future<ApiResponse> get(String path, {Map<String, dynamic>? params}) async {
+  static Future<ApiResponse> get(String path,
+      {Map<String, dynamic>? params}) async {
     try {
       final response = await _dio.get(path, queryParameters: params);
 
@@ -54,9 +57,10 @@ class ApiClient {
   }
 
   // POST
-  static Future<ApiResponse> post(String path, {Map<String, dynamic>? body}) async {
+  static Future<ApiResponse> post(String path,
+      {Map<String, dynamic>? body}) async {
     try {
-      final response =  await _dio.post(path, data: body);
+      final response = await _dio.post(path, data: body);
 
       return ApiResponse<dynamic>.fromJson(response.data, (json) => json);
     } catch (e) {
@@ -65,9 +69,10 @@ class ApiClient {
   }
 
   // PATCH
-  static Future<ApiResponse> patch(String path, {Map<String, dynamic>? body}) async {
+  static Future<ApiResponse> patch(String path,
+      {Map<String, dynamic>? body}) async {
     try {
-      final response =  await _dio.patch(path, data: body);
+      final response = await _dio.patch(path, data: body);
 
       return ApiResponse<dynamic>.fromJson(response.data, (json) => json);
     } catch (e) {
@@ -76,9 +81,10 @@ class ApiClient {
   }
 
   // PUT
-  static Future<ApiResponse> put(String path, {Map<String, dynamic>? body}) async {
+  static Future<ApiResponse> put(String path,
+      {Map<String, dynamic>? body}) async {
     try {
-      final response =  await _dio.put(path, data: body);
+      final response = await _dio.put(path, data: body);
 
       return ApiResponse<dynamic>.fromJson(response.data, (json) => json);
     } catch (e) {
@@ -87,9 +93,10 @@ class ApiClient {
   }
 
   // DELETE
-  static Future<ApiResponse> delete(String path, {Map<String, dynamic>? body}) async {
+  static Future<ApiResponse> delete(String path,
+      {Map<String, dynamic>? body}) async {
     try {
-      final response =  await _dio.delete(path, data: body);
+      final response = await _dio.delete(path, data: body);
 
       return ApiResponse<dynamic>.fromJson(response.data, (json) => json);
     } catch (e) {
