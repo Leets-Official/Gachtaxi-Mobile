7977cbbd1db64e356e11523fc820512afb3078ac
WooGi1020 <jungwk1020@gmail.com>
2025-04-01 22:35:00 +0900

    feat: 매칭리스트 프로바이더 재사용 가능하도록 수정

--- lib/domain/home/providers/response/manual_matching_data_provider.dart
+++ lib/domain/home/providers/response/manual_matching_data_provider.dart
@@ -1,74 +1,73 @@
 import 'package:gachtaxi_app/common/model/api_response.dart';
 import 'package:gachtaxi_app/domain/home/model/manual-matching/manual_matching_response_model.dart';
-import 'package:gachtaxi_app/domain/home/services/manual_matching_room_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'manual_matching_data_provider.g.dart';
 
 @riverpod
-class ManualMatchingDataNotifier extends _$ManualMatchingDataNotifier {
+class MatchingDataNotifier extends _$MatchingDataNotifier {
   final int _pageSize = 5;
+  bool _isFetchingMore = false;
+  late Future<ApiResponse<MatchingData>> Function(int, int) _fetchService;
+  int _currentPage = 0;
+  bool _hasMoreData = true;
 
   @override
-  Future<ApiResponse<MatchingData>> build() async {
-    return _fetchData(0);
+  Future<ApiResponse<MatchingData>> build(
+      Future<ApiResponse<MatchingData>> Function(int, int) fetchService) async {
+    _fetchService = fetchService;
+    _currentPage = 0;
+    _hasMoreData = true;
+    return _fetchData(_currentPage);
   }
 
   Future<ApiResponse<MatchingData>> _fetchData(int pageNumber) async {
-    final response = await ManualMatchingRoomService.fetchMatchingRooms(
-        pageNumber, _pageSize);
-
-    if (response == null) {
-      throw Exception('매칭방 로딩 중 에러 발생');
-    }
-
-    return response;
+    return await _fetchService(pageNumber, _pageSize);
   }
 
   Future<void> fetchMoreData() async {
-    if (state is AsyncData<ApiResponse<MatchingData>>) {
-      final currentData = state.value!;
-      final currentPageable = currentData.data?.pageable;
+    if (_isFetchingMore || !_hasMoreData) return;
+    _isFetchingMore = true;
 
-      if (currentPageable == null || currentPageable.last) {
-        return;
-      }
+    try {
+      final nextPage = _currentPage + 1;
+      final nextResponse = await _fetchData(nextPage);
 
-      final nextPage = currentPageable.pageNumber + 1;
+      final currentData = state.valueOrNull;
+      final updatedRooms = [
+        ...?currentData?.data?.rooms,
+        ...?nextResponse.data?.rooms,
+      ];
 
-      try {
-        final nextResponse = await _fetchData(nextPage);
+      _currentPage = nextPage;
+      _hasMoreData = !nextResponse.data!.pageable.last;
 
-        final updatedRooms = [
-          ...?currentData.data?.rooms,
-          ...?nextResponse.data?.rooms,
-        ];
-        final updatedPageable = nextResponse.data!.pageable;
+      final updatedMatchingData = MatchingData(
+        rooms: updatedRooms,
+        pageable: nextResponse.data!.pageable,
+      );
 
-        final updatedMatchingData = MatchingData(
-          rooms: updatedRooms,
-          pageable: updatedPageable,
-        );
+      final mergedApiResponse = ApiResponse(
+        code: nextResponse.code,
+        message: nextResponse.message,
+        data: updatedMatchingData,
+      );
 
-        final mergedApiResponse = ApiResponse(
-          code: nextResponse.code,
-          message: nextResponse.message,
-          data: updatedMatchingData,
-        );
-
-        state = AsyncData(mergedApiResponse);
-      } catch (e, stackTrace) {
-        state = AsyncError<ApiResponse<MatchingData>>(e, stackTrace)
-            .copyWithPrevious(state);
-      }
+      state = AsyncData(mergedApiResponse);
+    } catch (e, stackTrace) {
+      state = AsyncError<ApiResponse<MatchingData>>(e, stackTrace)
+          .copyWithPrevious(state);
+    } finally {
+      _isFetchingMore = false;
     }
   }
 
-  // 새로고침 함수
   Future<void> refresh() async {
     state = const AsyncLoading();
     try {
       final response = await _fetchData(0);
+      _currentPage = 0;
+      _hasMoreData = true;
       state = AsyncData(response);
     } catch (e, stackTrace) {
       state = AsyncError(e, stackTrace);
