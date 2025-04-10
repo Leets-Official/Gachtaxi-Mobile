400705363209d4c9ca6a6425f22d5b1b20cb7309
WooGi1020 <jungwk1020@gmail.com>
2025-04-11 00:31:12 +0900

    fix: tabView 로직 변경

--- lib/domain/home/view/tab-view/tab_view.dart
+++ lib/domain/home/view/tab-view/tab_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gachtaxi_app/common/util/slide_page_route.dart';
 import 'package:gachtaxi_app/domain/home/view/tab-view/auto_matching_screen.dart';
 import 'package:gachtaxi_app/domain/home/view/tab-view/friend_list_screen.dart';
 import 'package:gachtaxi_app/domain/home/view/tab-view/manual_matching_screen.dart';
-import 'package:gachtaxi_app/domain/my-page/view/my_page_screen.dart';
 
 class TabView extends StatefulWidget {
   const TabView({
@@ -18,37 +16,37 @@ class TabView extends StatefulWidget {
 }
 
 class _TabViewState extends State<TabView> with SingleTickerProviderStateMixin {
-  int _previousIndex = 0;
+  // int _previousIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    widget.tabController.addListener(_handleTabChange);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   widget.tabController.addListener(_handleTabChange);
+  // }
 
-  @override
-  void dispose() {
-    widget.tabController.removeListener(_handleTabChange);
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   widget.tabController.removeListener(_handleTabChange);
+  //   super.dispose();
+  // }
 
-  void _handleTabChange() {
-    if (widget.tabController.index == 3) {
-      _previousIndex =
-          _previousIndex != 3 ? widget.tabController.previousIndex : 0;
-      Future.microtask(() {
-        if (!mounted) return;
-        Navigator.of(context).push(
-          SlidePageRoute(screen: MyPageScreen()),
-        );
-        if (mounted) {
-          setState(() {
-            widget.tabController.index = _previousIndex; // 이전 탭으로 복귀
-          });
-        }
-      });
-    }
-  }
+  // void _handleTabChange() {
+  //   if (widget.tabController.index == 3) {
+  //     _previousIndex =
+  //         _previousIndex != 3 ? widget.tabController.previousIndex : 0;
+  //     Future.microtask(() {
+  //       if (!mounted) return;
+  //       Navigator.of(context).push(
+  //         SlidePageRoute(screen: MyPageScreen()),
+  //       );
+  //       if (mounted) {
+  //         setState(() {
+  //           widget.tabController.index = _previousIndex; // 이전 탭으로 복귀
+  //         });
+  //       }
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +57,7 @@ class _TabViewState extends State<TabView> with SingleTickerProviderStateMixin {
         AutoMatchingScreen(),
         ManualMatchingScreen(),
         FriendListScreen(),
-        Container(),
+        SizedBox.shrink(),
       ],
     );
   }
