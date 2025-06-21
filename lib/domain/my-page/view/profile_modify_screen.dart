52943ba1f7347ad86ee3baf215235060f385d657
WooGi1020 <jungwk1020@gmail.com>
2025-06-21 16:56:22 +0900

    feat: 회원정보 수정 로직 작성 및 적용

--- lib/domain/my-page/view/profile_modify_screen.dart
+++ lib/domain/my-page/view/profile_modify_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gachtaxi_app/common/components/button.dart';
 import 'package:gachtaxi_app/common/components/input_field.dart';
 import 'package:gachtaxi_app/common/constants/spacing.dart';
 import 'package:gachtaxi_app/common/layout/default_layout.dart';
 import 'package:gachtaxi_app/common/util/toast_show_utils.dart';
+import 'package:gachtaxi_app/domain/my-page/providers/my-info/info_modify_provider.dart';
 import 'package:gachtaxi_app/domain/sign-up/components/profile_upload.dart';
 
-class ProfileModifyScreen extends StatelessWidget {
+class ProfileModifyScreen extends ConsumerWidget {
   ProfileModifyScreen({super.key});
   final TextEditingController nicknameController = TextEditingController();
-  final TextEditingController accountController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final infoModifyState = ref.watch(infoModifyProvider);
+    final isLoading = infoModifyState is AsyncLoading;
+
     return DefaultLayout(
       title: '프로필 수정',
       hasAppBar: true,
@@ -39,10 +43,33 @@ class ProfileModifyScreen extends StatelessWidget {
                 child: SizedBox(),
               ),
               Button(
+                isLoading: isLoading,
                 buttonText: '변경하기',
-                onPressed: () {
-                  Navigator.pop(context);
-                  ToastShowUtils(context: context).showSuccess('변경되었어요');
+                onPressed: () async {
+                  final nickName = nicknameController.text.trim();
+                  final profile = null; // 프로필 아바타 컴포넌트 완성 후 처리
+
+                  if (nickName.isEmpty && profile == null) {
+                    ToastShowUtils(context: context)
+                        .showSuccess('변경된 부분이 없어요!');
+                    return;
+                  }
+
+                  try {
+                    await ref
+                        .read(infoModifyProvider.notifier)
+                        .updateInfo(profile, nickName);
+
+                    if (context.mounted) {
+                      Navigator.pop(context);
+                      ToastShowUtils(context: context).showSuccess('변경되었어요');
+                    }
+                  } catch (e) {
+                    if (context.mounted) {
+                      ToastShowUtils(context: context)
+                          .showSuccess('변경 실패: ${e.toString()}');
+                    }
+                  }
                 },
               ),
             ],
