e930cac327a7f79532e3cc2bf624620056db307a
우기 <jungwk1020@gmail.com>
2025-06-29 20:25:10 +0900

    feat: 임시 로직

--- lib/domain/matching-waiting/view/matching_waiting_screen.dart
+++ lib/domain/matching-waiting/view/matching_waiting_screen.dart
@@ -1,64 +1,121 @@
 import 'package:flutter/material.dart';
-import 'package:gachtaxi_app/common/components/button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gachtaxi_app/common/constants/spacing.dart';
 import 'package:gachtaxi_app/common/constants/typography.dart';
 import 'package:gachtaxi_app/common/layout/default_layout.dart';
+import 'package:gachtaxi_app/common/model/api_response.dart';
+import 'package:gachtaxi_app/common/util/slide_page_route.dart';
+import 'package:gachtaxi_app/common/util/toast_show_utils.dart';
+import 'package:gachtaxi_app/domain/home/model/auto-matching/auto_matching_request_model.dart';
+import 'package:gachtaxi_app/domain/home/providers/request/auto_matching_provider.dart';
+import 'package:gachtaxi_app/domain/home/providers/ui/matching_setting_provider.dart';
+import 'package:gachtaxi_app/domain/home/services/get_current_location_service_provider.dart';
+import 'package:gachtaxi_app/domain/my-page/view/account_number_modify_screen.dart';
 import 'package:lottie/lottie.dart';
 
-class MatchingWaitingScreen extends StatelessWidget {
+class MatchingWaitingScreen extends ConsumerStatefulWidget {
   const MatchingWaitingScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return DefaultLayout(
-      hasAppBar: true,
-      child: SafeArea(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '가치 탈 인원을\n 찾는 중이에요!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: AppTypography.fontSizeExtraLarge,
-                        fontWeight: AppTypography.fontWeightSemibold,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: AppSpacing.spaceMedium,
-                    ),
-                    Text(
-                      '3/4',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: AppTypography.fontSizeLarge,
-                        fontWeight: AppTypography.fontWeightSemibold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Lottie.asset(
-                'assets/images/taxi_loading.json',
-                width: double.infinity,
-                height: 320,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: AppSpacing.spaceCommon),
-                  child: Center(child: Button(buttonText: '채팅방')),
-                ),
-              ),
-            ]),
-      ),
+  ConsumerState<MatchingWaitingScreen> createState() =>
+      _MatchingWaitingScreenState();
+}
+
+class _MatchingWaitingScreenState extends ConsumerState<MatchingWaitingScreen> {
+  late final ProviderSubscription _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _subscription = ref.listenManual<AsyncValue<ApiResponse?>>(
+      autoMatchingProvider,
+      (prev, next) {
+        debugPrint('listen triggered: $next');
+        if (next is AsyncData && next.value != null) {
+          if (mounted) {
+            Navigator.of(context).pushReplacement(
+              SlidePageRoute(screen: AccountNumberModifyScreen()),
+            );
+          }
+        } else if (next is AsyncError) {
+          ToastShowUtils(context: context).showSuccess('자동매칭 요청에 실패했어요.');
+        }
+      },
     );
+
+    _startAutoMatching();
   }
+
+  Future<void> _startAutoMatching() async {
+    final matchingState = ref.read(matchingSettingNotifierProvider);
+    final locationService = ref.read(getCurrentLocationServiceProvider);
+    final autoMatchingService = ref.read(autoMatchingProvider.notifier);
+
+    final currentPosition = await locationService.getCurrentLocation();
+    final startLngLat =
+        '${currentPosition.longitude}, ${currentPosition.latitude}';
+
+    final destination = matchingState.destination;
+    final destiLngLat = '${destination.longitude}, ${destination.latitude}';
+
+    final requestBody = AutoMatchingRequest(
+      startPoint: startLngLat,
+      destinationPoint: destiLngLat,
+      criteria: matchingState.selectedTags.toList(),
+      members: matchingState.selectedFriends.toList(),
+      expectedTotalCharge: 4000,
+      destinationName: destination.name,
+      departure: null,
+      destination: null,
+      startName: matchingState.departure.name,
+    );
+
+    await autoMatchingService.requestAutoMatching(requestBody);
+  }
+
+  @override
+  void dispose() {
+    _subscription.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildLoadingScreen();
+  }
+}
+
+Widget _buildLoadingScreen() {
+  return DefaultLayout(
+    hasAppBar: true,
+    child: SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '가치 탈 인원을\n 찾는 중이에요!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: AppTypography.fontSizeExtraLarge,
+                  fontWeight: AppTypography.fontWeightSemibold,
+                ),
+              ),
+              const SizedBox(height: AppSpacing.spaceMedium),
+            ],
+          ),
+          Lottie.asset(
+            'assets/images/taxi_loading.json',
+            width: double.infinity,
+            height: 320,
+          ),
+        ],
+      ),
+    ),
+  );
 }
